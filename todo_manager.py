352f19a67d6baeb99fb55d388b127f1d0aeb40f9
John1Kapika <vano2001878@gmail.com>
2026-08-17 11:03:59 +0300

    Реализован метод load

--- todo_manager.py
+++ todo_manager.py
@@ -24,17 +24,32 @@ class TaskManager:
         self.next_id = 1
         self.load()
 
-    def find_task(self):
-        def find_task(self, task_id):
-            for task in self.tasks:
-                if task.id == task_id:
-                    return task
-            return None
+    def find_task(self, task_id):
+        for task in self.tasks:
+            if task.id == task_id:
+                return task
+        return None
 
     def save(self):
         with open('tasks.json', 'w', encoding='utf-8') as f:
             json.dump([task.to_dict() for task in self.tasks], f, indent=2, ensure_ascii=False)
 
+    def load(self):
+        try:
+            with open('tasks.json', 'r', encoding='utf-8') as f:
+                data = json.load(f)
+            self.tasks = [Task.from_dict(item) for item in data]
+            if self.tasks:
+                self.next_id = max(task.id for task in self.tasks) + 1
+            else:
+                self.next_id = 1
+        except FileNotFoundError:
+            self.tasks = []
+            self.next_id = 1
+        except json.JSONDecodeError:
+            print("Файл tasks.json повреждён. Будет создан новый список задач.")
+            self.tasks = []
+            self.next_id = 1
     def add(self, title):
         # TODO: создать задачу, добавить, сохранить
 
